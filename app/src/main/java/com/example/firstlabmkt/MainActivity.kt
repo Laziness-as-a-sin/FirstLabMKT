d26fc79c9df4f1db9e0fffe8acd03a4e88677e3c
Pavel Kim <pavel20125@gmail.com>
2022-09-27 10:49:34 +1000

    button fun

--- app/src/main/java/com/example/firstlabmkt/MainActivity.kt
+++ app/src/main/java/com/example/firstlabmkt/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.firstlabmkt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+//        val button:Button = findViewById(R.id.button);
         setContentView(R.layout.activity_main)
+
+        super.onCreate(savedInstanceState)
+        textView  = findViewById(R.id.textView)
     }
+
+    fun hello(view: View){
+        textView.setText("Hello there!")
+        val tempTost = Toast.makeText(this, "Hello!", Toast.LENGTH_SHORT)
+        tempTost.show()
+    }
+
 }
